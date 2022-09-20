fcdc93ce157ed1b96cee4fcdb874bd855d1b0b52
Savakar Rohan <rohansavakar@gmail.com>
2022-09-20 14:09:04 +0530

    About page self added

--- pages/views.py
+++ pages/views.py
@@ -4,3 +4,6 @@ class HomePageView(TemplateView):
     """Home page view"""
     template_name: str = "home.html"
     
+class AboutPageView(TemplateView):
+    """About page view"""
+    template_name: str = "about.html"
