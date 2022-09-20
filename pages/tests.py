e9eb8a1b1bebafdf688c0002c67d959237e8b80e
Savakar Rohan <rohansavakar@gmail.com>
2022-09-20 14:45:54 +0530

    Added simple tests

--- pages/tests.py
+++ pages/tests.py
@@ -1,17 +1,38 @@
 from urllib import response
 from django.test import SimpleTestCase
+from django.urls import reverse
 
 # Create your tests here.
 class HomepageTests(SimpleTestCase):
-    """Test for home page URL being root URL"""
+    """Test for home page"""
     def test_url_exists_at_correct_location(self):
-        """test Url"""
-        response1 = self.client.get("/")
-        self.assertEqual(response1.status_code,200)
+        """test URL of homepage"""
+        response = self.client.get("/")
+        self.assertEqual(response.status_code,200)
+        
+    def test_url_available_by_name(self):
+        """ test if URL is available by name"""
+        response = self.client.get(reverse("home"))
+        self.assertEqual(response.status_code,200)
+        
+    def test_template_name_correct(self):
+        """test if the template is correct template"""
+        response = self.client.get(reverse("home"))
+        self.assertTemplateUsed(response, "home.html")
          
 class AboutpageTests(SimpleTestCase):
-    """Tests for the about page"""
+    """Tests for the aboutpage"""
     def test_url_exists_at_correct_location(self):
         """Test URL at correct location"""
-        response2 = self.client.get("/about/")
-        self.assertEqual(response2.status_code,200)
+        response = self.client.get("/about/")
+        self.assertEqual(response.status_code,200)
+        
+    def test_url_available_by_name(self):
+        """ test if URL is available by name"""
+        response = self.client.get(reverse("about"))
+        self.assertEqual(response.status_code,200)
+        
+    def test_template_name_correct(self): 
+        """test if the template is correct template"""
+        response = self.client.get(reverse("home"))
+        self.assertTemplateUsed(response, "home.html")
